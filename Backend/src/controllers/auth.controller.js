a7b4a93e4bcf6d3f73a93482a6eabc8265e43738
Sharayu Shelke <sharayushelke2006@gmail.com>
2025-12-20 21:44:48 +0530

    login logout functions added

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -56,5 +56,34 @@ export const signup= async(req,res)=>{
     }
 };
 export const login= async(req,res)=>{
-    res.send("Login route");
-};          
+    const {email,password}=req.body;
+    try{
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        } 
+        const user= await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid credentials"});
+        }  
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        generateToken(user._id,res);
+        return res.status(200).json({_id:user._id,fullName:user.fullName,email:user.email,profilePic:user.profilePic});     
+}
+catch(err){
+    console.log("Error in login:",err);
+    return res.status(500).json({message:"Server error"});
+}
+}
+
+
+
+export const logout= async(_,res)=>{
+    res.cookie("jwt","",{
+        maxAge:0,}
+    );
+
+    return res.status(200).json({message:"Logged out successfully"});
+};
