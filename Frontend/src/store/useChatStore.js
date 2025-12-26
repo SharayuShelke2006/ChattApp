7c716ebc6d8367aea811eeb2188fd37e234ad87c
Sharayu Shelke <sharayushelke2006@gmail.com>
2025-12-26 13:41:05 +0530

    chat container ui before socket implementation

--- Frontend/src/store/useChatStore.js
+++ Frontend/src/store/useChatStore.js
@@ -48,5 +48,39 @@ export const useChatStore = create((set, get) => ({
       set({ isUserLoading: false });
     }
   },
+  getMessagesByUserID: async (userID) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userID}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to fetch messages");
+    } finally {
+      set({ isMessagesLoading: false });
+    }  },
 
+    sendMessage: async(messageData) =>{
+      const {selectedUser,messages}=get();
+      const {authUser}=useAuthorStore.getState();
+      const tempId=`temp-${Date.now()}`;
+      const optimisticMessage = {
+        _id:tempId,
+        senderId: authUser._id,
+        reveiverId:selectedUser._id,
+        text:messageData.text,
+        image:messageData.image,
+        createdAt : new Date().toISOString(),
+        isOptimistic:true,
+      }
+      //immediately update ui using msg
+      set({messages:[...messages,optimisticMessage]})
+      try{
+        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`,messageData);
+        set({messages:messages.concat(res.data)})
+      }catch(error){
+        //remove optimistic msg on error
+        set({messages:messages})
+        toast.error(error.response?.data?.message || "Failed to send messages");
+      }
+    }
 }));
